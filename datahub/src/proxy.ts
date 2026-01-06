cefa616baa4a504753fc76972ef8f4123086e14d
mateus-rcosta <140670436+mateus-rcosta@users.noreply.github.com>
2026-01-06 15:01:19 -0300

    corrigido permissão de acesso

--- datahub/src/proxy.ts
+++ datahub/src/proxy.ts
@@ -1,27 +1,54 @@
 import { NextRequest, NextResponse } from "next/server";
 import { verificaSessao } from "./lib/sessao";
-import { SessaoError } from "./lib/sessao-error";
 
 const publicRoutes = ["/auth/login", "/"];
+const authenticatedRoutes = ["/dashboard"];
+
+const pathPermissoes = {
+  "editar_base_dados":["/api/base-dados", "/base-dados", "/api/cliente"],
+  "editar_integracoes":["/api/integracoes", "/integracoes"],
+  "visualizar_relatorios":["/api/relatorios", "/relatorios"],
+  "editar_campanhas":["/api/campanhas", "/campanhas"],
+  "admin":["/api/usuarios", "/usuarios"]
+}
 
 export default async function proxy(req: NextRequest) {
   const path = req.nextUrl.pathname;
-  const isPublicRoute = publicRoutes.includes(path);
 
-  if (isPublicRoute || req.headers.get("next-action")) {
+  if (publicRoutes.includes(path) || req.headers.get("next-action")) {
     return NextResponse.next();
   }
 
   try {
-    await verificaSessao();
-    return NextResponse.next();
-  } catch (error: unknown) {
-    if (error instanceof SessaoError) {
-        return NextResponse.redirect(new URL("/auth/login", req.url));
+    const sessao = await verificaSessao();
+    if (authenticatedRoutes.some(p => path.startsWith(p))) {
+      return NextResponse.next();
     }
 
+    const autorizado = temPermissaoParaPath(path, sessao.permissoes, sessao.admin);
+
+    if (!autorizado) {
+      return NextResponse.redirect(new URL("/auth/login", req.url));
+    }
+
+    return NextResponse.next();
+  } catch {
     return NextResponse.redirect(new URL("/auth/login", req.url));
   }
+}
+function temPermissaoParaPath( path: string, permissoesUsuario: string[], admin: boolean): boolean {
+  if (admin) return true;
+
+  for (const permissao of permissoesUsuario) {
+    const pathsPermitidos = pathPermissoes[permissao as keyof typeof pathPermissoes];
+    if (!pathsPermitidos) continue;
+
+    if (pathsPermitidos.some((p) => path.startsWith(p))) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 export const config = {
